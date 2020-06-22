4126b1eb0a40c5f249d1c92146df15375892312d
hakshu <flyhighup.air25@gmail.com>
2020-06-22 17:53:36 +0900

    Fix test

--- spec/requests/books_request_spec.rb
+++ spec/requests/books_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Books", type: :request do
 
   describe "GET /index" do
     it "returns http success" do
-      get "/books/index"
+      get books_path
       expect(response).to have_http_status(:success)
     end
   end
