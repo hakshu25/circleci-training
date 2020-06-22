3dd2bc37e0724d3a5a825830a61ccd1a62b88cf1
hakshu <flyhighup.air25@gmail.com>
2020-06-22 17:06:56 +0900

    Add delete button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'books#index'
 
-  resources :books, only: [:index, :new, :create, :edit, :update]
+  resources :books, except: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
