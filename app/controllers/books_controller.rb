e3997167eb51dbea4c9e9ed61d67aa865da73ce7
hakshu <flyhighup.air25@gmail.com>
2020-06-22 16:44:34 +0900

    Add book create page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    if Book.create!(book_params)
+      redirect_to '/books'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
 end
